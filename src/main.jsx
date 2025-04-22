79338359bec9246878c3ae37156f01243bdd2c79
Kenny <kenny@audico.co.za>
2025-04-22 08:55:07 +0200

    fix: simplify and clean up main.jsx structure

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';
 import AudicoQuoteWidget from './widget.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <AudicoQuoteWidget />
   </React.StrictMode>
