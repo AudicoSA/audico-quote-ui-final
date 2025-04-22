46aa97b618d5ad2f14e9f14bda22504c41663513
AudicoSA <112856045+AudicoSA@users.noreply.github.com>
2025-04-22 08:48:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+"use client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AudicoQuoteWidget from './widget.jsx';
+import './index.css';
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AudicoQuoteWidget />
+  </React.StrictMode>
+);
